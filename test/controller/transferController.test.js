78fb5e1aad0f09d4f779aba195f9a3eaac1a0bf5
Juliana Rocha <julianasantorocha12@gmail.com>
2025-09-11 11:52:30 +0300

    Adicionando um novo erro na transferencia com Mock

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -6,25 +6,46 @@ const { expect } = require('chai');
 //Aplicacao
 const app = require('../../app')
 
+//Mock
+const transferService = require('../../service/transferService')
+
 // Testes
 describe('Transfer Controller', () => {
-    describe ('POST /transfer', () => {
+    describe('POST /transfer', () => {
         it('Quando informo destinatarios inexistente recebo 400', async () => {
             const resposta = await request(app)
-            .post('/transfer')
-            .send({
-               from: "Juliana", 
-               to: "Pedro",
-               value: 100
+                .post('/transfer')
+                .send({
+                    from: "Juliana",
+                    to: "Pedro",
+                    value: 100
 
-            });
-        expect(resposta.status).to.equal(400);
-        expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado');
-        
+                });
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado');
+
+        });
+
+        it('Usando Mocks: Quando informo destinatarios inexistente recebo 400', async () => {
+            //mocar apenas a funcao transfer do service
+            const transferServiceMock = sinon.stub(transferService, 'transfer')
+            transferServiceMock.throws(new Error('Usuário remetente ou destinatário não encontrado'))
+
+            const resposta = await request(app)
+                .post('/transfer')
+                .send({
+                    from: "juliana",
+                    to: "pedro",
+                    value: 100
+
+                });
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado');
+
         });
     });
 
-    describe ('GET /transfers', () => {
+    describe('GET /transfers', () => {
         //its ficam aqui
 
     });
